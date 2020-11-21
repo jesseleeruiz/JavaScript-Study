27fb63f412af46eabdbefeb4e216acf6bccf7db6
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-20 15:50:58 -1000

    Completed lecture on functions.

--- 02-Fundamentals-Part-2 copy/starter/script.js
+++ 02-Fundamentals-Part-2 copy/starter/script.js
@@ -1,8 +1,39 @@
 "use strict";
-
+/*
+LESSON 1 - STRICT MODE
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 
 if (hasDriversLicense) console.log("I can drive.");
+*/
+
+/*
+LECTURE 2 - FUNCTIONS - A piece of code we can reuse over and over again.
+
+
+function logger() {
+    console.log("My name is Jesse.");
+}
+
+logger(); // Invoking, running, or calling the function.
+
+// Usually pass data and return data in a function
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleAndOrangeJuice = fruitProcessor(2, 4);
+console.log(appleAndOrangeJuice);
+*/
+
+/*
+LECTURE 3 - FUNCTION DECLARATIONS & EXPRESSIONS
+*/
+
