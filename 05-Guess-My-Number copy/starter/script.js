6299bfa447cc6d7b052abb57eadfa9cb1c4b27ee
jesseleeruiz <jesseleeruiz@gmail.com>
2020-12-01 16:42:08 -1000

    Learned about handling click events.

--- 05-Guess-My-Number copy/starter/script.js
+++ 05-Guess-My-Number copy/starter/script.js
@@ -1,8 +1,22 @@
 'use strict';
 
+// DOM Manipulation
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 5;
+*/
+
+// Handling Click Events
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      '⛔️ No number! Please enter a number.';
+  }
+});
