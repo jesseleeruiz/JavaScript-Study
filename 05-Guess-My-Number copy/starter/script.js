66621317c1b2723699baa299f3df5e5b4f70b041
jesseleeruiz <jesseleeruiz@gmail.com>
2020-12-01 17:01:11 -1000

    Added some game logic.

--- 05-Guess-My-Number copy/starter/script.js
+++ 05-Guess-My-Number copy/starter/script.js
@@ -10,6 +10,11 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 5;
 */
 
+// Game Logic - Defining random number as game starts
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 // Handling Click Events
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,5 +23,25 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     document.querySelector('.message').textContent =
       '⛔️ No number! Please enter a number.';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'The guess is too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'The guess is too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
