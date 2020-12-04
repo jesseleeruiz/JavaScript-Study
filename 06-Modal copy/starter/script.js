77e937a5126a66f4a35269192c860bf9f89ced4e
jesseleeruiz <jesseleeruiz@gmail.com>
2020-12-04 12:12:22 -1000

    Learned about the modal window project. Learned about querySelectorAll to select all elements with the same class name.

--- 06-Modal copy/starter/script.js
+++ 06-Modal copy/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalButton = document.querySelector('.close-modal');
+const openModelButtons = document.querySelectorAll('.show-modal'); // Selects all the elements w/ that class
+
+for (let i = 0; i < openModalButtons.length; i++)
+  console.log(openModelButtons[i].textContent);
