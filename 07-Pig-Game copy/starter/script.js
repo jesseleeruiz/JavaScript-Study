a7062f15836f7854b697d19a6cd7803e1a7413e4
jesseleeruiz <jesseleeruiz@gmail.com>
2020-12-07 11:43:00 -1000

    Selected the scores and the dice and started them at their initial values.

--- 07-Pig-Game copy/starter/script.js
+++ 07-Pig-Game copy/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const score0 = document.querySelector('#score--0'); // Hash is selector for id
+const score1 = document.getElementById('score--1'); // Same result, just different way.
+const diceElemenet = document.querySelector('.dice');
+
+score0.textContent = 0;
+score1.textContent = 0;
+diceElemenet.classList.add('hidden');
